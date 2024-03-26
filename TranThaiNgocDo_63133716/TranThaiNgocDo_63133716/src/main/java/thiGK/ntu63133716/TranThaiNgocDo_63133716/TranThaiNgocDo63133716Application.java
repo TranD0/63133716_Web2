065998b0cc5a3bf9368f9b1f8e92e173ecfd2a14
TranD0 <trthaingocdo@gmail.com>
2024-03-26 14:57:32 +0700

    làm bài giữa kì

---
+++ TranThaiNgocDo_63133716/TranThaiNgocDo_63133716/src/main/java/thiGK/ntu63133716/TranThaiNgocDo_63133716/TranThaiNgocDo63133716Application.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63133716.TranThaiNgocDo_63133716;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranThaiNgocDo63133716Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranThaiNgocDo63133716Application.class, args);
+	}
+
+}
---
+++ TranThaiNgocDo_63133716/TranThaiNgocDo_63133716/src/test/java/thiGK/ntu63133716/TranThaiNgocDo_63133716/TranThaiNgocDo63133716ApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63133716.TranThaiNgocDo_63133716;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranThaiNgocDo63133716ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
