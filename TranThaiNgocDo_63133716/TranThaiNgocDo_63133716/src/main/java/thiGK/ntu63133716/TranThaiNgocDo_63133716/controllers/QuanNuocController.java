5bd021725ea4aafa0af4dd4e3ac14c59df477ee9
TranD0 <trthaingocdo@gmail.com>
2024-03-26 16:25:44 +0700

    làm bài giữa kì

--- TranThaiNgocDo_63133716/TranThaiNgocDo_63133716/src/main/java/thiGK/ntu63133716/TranThaiNgocDo_63133716/controllers/QuanNuocController.java
+++ TranThaiNgocDo_63133716/TranThaiNgocDo_63133716/src/main/java/thiGK/ntu63133716/TranThaiNgocDo_63133716/controllers/QuanNuocController.java
@@ -1,15 +1,17 @@
 package thiGK.ntu63133716.TranThaiNgocdo_63133716.controllers;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import thiGK.ntu63133716.TranThaiNgocdo_63133716.models.DTOQuanNuoc;
 public class QuanNuocController {
-	 @GetMapping("/Index")
+	 @GetMapping("/Home")
 	    public String showIndexPage() {
 	        return "Index";
 	    }
@@ -22,6 +24,18 @@ public class QuanNuocController {
 		@PostMapping("/themMoiQN")
 		public String submitForm(@ModelAttribute("qnDTO") DTOQuanNuoc qn) {
 		    return "themQuanNuoc_OK";
+		}
+		
+		static List<DTOQuanNuoc> dsDoUong=  new ArrayList<DTOQuanNuoc>();
+		static {
+			dsDoUong.add(new DTOQuanNuoc("0001", "cà phê",5,200));
+			dsDoUong.add(new DTOQuanNuoc("0002", "nước lọc",4,200));
+			 }
+
+		@GetMapping("/danhsachDU")
+		public String listStudent(ModelMap mm) {
+		    mm.addAttribute("dsSV", dsDoUong);
+		    return "douong_getAll";
 		}
 
 }
--- TranThaiNgocDo_63133716/TranThaiNgocDo_63133716/src/main/java/thiGK/ntu63133716/TranThaiNgocDo_63133716/models/DTOQuanNuoc.java
+++ TranThaiNgocDo_63133716/TranThaiNgocDo_63133716/src/main/java/thiGK/ntu63133716/TranThaiNgocDo_63133716/models/DTOQuanNuoc.java
@@ -2,4 +2,52 @@ package thiGK.ntu63133716.TranThaiNgocdo_63133716.models;
 
 public class DTOQuanNuoc {
 
+	    private String maDoUong;
+	    private String tenDoUong;
+	    private int soLuong;
+	    private double donGia;
+
+	    public DTOQuanNuoc(String maDoUong, String tenDoUong, int soLuong, double donGia) {
+	        this.maDoUong = maDoUong;
+	        this.tenDoUong = tenDoUong;
+	        this.soLuong = soLuong;
+	        this.donGia = donGia;
+	    }
+	    public DTOQuanNuoc() {
+	    	super();
+	    }
+
+	    // Getters and setters
+	    public String getMaDoUong() {
+	        return maDoUong;
+	    }
+
+	    public void setMaDoUong(String maDoUong) {
+	        this.maDoUong = maDoUong;
+	    }
+
+	    public String getTenDoUong() {
+	        return tenDoUong;
+	    }
+
+	    public void setTenDoUong(String tenDoUong) {
+	        this.tenDoUong = tenDoUong;
+	    }
+
+	    public int getSoLuong() {
+	        return soLuong;
+	    }
+
+	    public void setSoLuong(int soLuong) {
+	        this.soLuong = soLuong;
+	    }
+
+	    public double getDonGia() {
+	        return donGia;
+	    }
+
+	    public void setDonGia(double donGia) {
+	        this.donGia = donGia;
+	    }
+
 }
