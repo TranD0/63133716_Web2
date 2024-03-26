f3874020cbd88d3d646e1f8b4562bca0d21f2736
TranD0 <trthaingocdo@gmail.com>
2024-03-26 16:56:04 +0700

    làm bài giữa kì

--- TranThaiNgocDo_63133716/TranThaiNgocDo_63133716/src/main/java/thiGK/ntu63133716/TranThaiNgocDo_63133716/controllers/QuanNuocController.java
+++ TranThaiNgocDo_63133716/TranThaiNgocDo_63133716/src/main/java/thiGK/ntu63133716/TranThaiNgocDo_63133716/controllers/QuanNuocController.java
@@ -1,6 +1,7 @@
 package thiGK.ntu63133716.TranThaiNgocdo_63133716.controllers;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import thiGK.ntu63133716.TranThaiNgocdo_63133716.models.DTOQuanNuoc;
+@Controller
 public class QuanNuocController {
 	 @GetMapping("/Home")
 	    public String showIndexPage() {
@@ -18,7 +21,7 @@ public class QuanNuocController {
 	 @GetMapping("/themMoiQN")
 		public String showForm(Model mm) {
 	        DTOQuanNuoc quannuocNull = new DTOQuanNuoc();
-	        mm.addAttribute("svDTO", quannuocNull);
+	        mm.addAttribute("qnDTO", quannuocNull);
 	        return "themQuanNuoc_form";
 	    }
 		@PostMapping("/themMoiQN")
@@ -33,9 +36,20 @@ public class QuanNuocController {
 			 }
 
 		@GetMapping("/danhsachDU")
-		public String listStudent(ModelMap mm) {
-		    mm.addAttribute("dsSV", dsDoUong);
+		public String listdouong(ModelMap mm) {
+		    mm.addAttribute("dsSV",dsDoUong);
 		    return "douong_getAll";
 		}
+		 @GetMapping("/timkiem")
+		    public String search(ModelMap mm, @RequestParam("keyword") String keyword) {
+		        // Tìm kiếm các đồ uống dựa trên keyword
+		        List<DTOQuanNuoc> ketQuaTimKiem = dsDoUong.stream()
+		                .filter(douong -> douong.getTenDoUong().toLowerCase().contains(keyword.toLowerCase()))
+		                .collect(Collectors.toList());
+
+		        // Đưa kết quả tìm kiếm vào ModelMap
+		        mm.addAttribute("ketQuaTimKiem", ketQuaTimKiem);
+		        return "ketqua_timkiem";
+		    }
 
 }
